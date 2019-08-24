3ab1d6373f2edb078c1ec8e9328b3772092fbecd
김지훈 <swth1991@gmail.com>
2019-08-24 14:05:16 +0900

    simple bug fix

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,7 +15,7 @@ def members():
                             
 @app.route("/members/<string:name>/")
 def getMember(name):
-    return name</string:name>
-                                    
+    return name                                    
+
 if __name__ == "__main__":
     app.run()
